dc31fb52078d8dc712029bda25a4abec74916813
Ra <82923488+avalokia@users.noreply.github.com>
2022-10-30 20:44:23 +0700

    Pushed websitefrontend

---
+++ websitefrontend/src/App.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import './App.css';
+import BarChart from './components/BarChart';
+ import {TotalCustomerPerBranch,CustomerData} from './Data';
+import chart from 'chart.js/dist/chart';
+import Moment from 'react-moment';
+import 'moment-timezone';
+import LineChart from './components/LineChart';
+
+class App extends React.Component {
+
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  //ComponentDidMount is used to execute the code
+  componentDidMount() {
+    fetch("http://localhost:3000/api/test2")
+    .then((res) => res.json())
+    .then((json) => {
+      this.setState({
+        items: json,
+        DataisLoaded: true
+      });
+    })
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded) return <div> <h1> Please wait some time... </h1></div>;
+    let today = new Date(); //Date(2022,3,5);
+    let firstDayOfTheWeek = new Date(today.getFullYear(),today.getMonth(),today.getDate()-today.getDay()+1); //start from monday
+    let lastDayOfTheWeek = new Date(today.getFullYear(),today.getMonth(),today.getDate()-today.getDay()+7);
+    let fd = firstDayOfTheWeek.toLocaleString('id-ID');
+    let ld = lastDayOfTheWeek.toLocaleString('id-ID');
+    //console.log(today + ' ' + firstDayOfTheWeek + ' ' + lastDayOfTheWeek);
+    //console.log('fd = ' + fd);
+    for (let j = firstDayOfTheWeek; j < lastDayOfTheWeek; j.setDate(j.getDate()+1)){
+      console.log('j = ' + j);
+      //console.log(items.data.length);
+      for (let i = 0; i < items.data.length; i++){
+        console.log(new Date(items.data[i].Timestamp));
+        //console.log(new Date(items.data[i].Timestamp) + ' > ' + j)
+        if (new Date(items.data[i].Timestamp) > j && new Date(items.data[i].Timestamp) < j.getDate()+1) {
+          console.log('items.data[i].Timestamp' + items.data[i].Timestamp + ' > ' + j)
+        } 
+      }
+    }
+
+    return (
+      <div className = "App">
+        
+        <h1> Vessel Barbershop </h1>
+        <p> Current Date = { String(today) } </p>
+        <p> Start of The Week = { fd } </p>
+        <p> End of The Week = {ld} </p>
+
+        <p>Customers of the week = { items.customerOfTheWeek }</p>
+        {
+          items.data.map((item) => (
+            <div> Timestamp = {item.Timestamp},  
+            DeviceID = {item.DeviceID}, 
+            Customer duration = {item.Duration} </div>
+          ))}
+      </div>
+    )
+  }
+
+}
+
+export default App;
+
+export function BarChartApp() {
+
+  const xAxisData = TotalCustomerPerBranch.map((data) => data.day); //retrieve x-axis data
+  const yAxisData = TotalCustomerPerBranch.map((data) => data.total); //retrieve y-axis data
+  // console.log(xAxisData);
+  // console.log(yAxisData);
+  
+  return (
+    <div className="BarChartApp"> 
+    <h1>Total Pelanggan Per Cabang</h1>
+    <BarChart xAxisData={xAxisData} yAxisData={yAxisData}/> 
+    </div>
+  );
+}
+
+export function LineChartApp() {
+
+  const xAxisData = CustomerData.map((data) => data.day); //retrieve x-axis data
+  const yAxisData = CustomerData.map((data) => data.total); //retrieve y-axis data
+  // console.log(xAxisData);
+  // console.log(yAxisData);
+  
+  return (
+    <div className="LineChartApp"> 
+    <h1> Total Pelanggan </h1>
+    <LineChart xAxisData={xAxisData} yAxisData={yAxisData}/> 
+    </div>
+  );
+}
---
+++ websitefrontend/src/Data.js
@@ -0,0 +1,75 @@
+export const CustomerData = [
+    {
+        id: 1,
+        day: 'Monday',
+        total: 7
+    },
+    {
+        id: 2,
+        day: 'Tuesday',
+        total: 8
+    },
+    {
+        id: 4,
+        day: 'Wednesday',
+        total: 6
+    },
+    {
+        id: 5,
+        day: 'Thursday',
+        total: 5
+    },
+    {
+        id: 6,
+        day: 'Friday',
+        total: 10
+    },
+    {
+        id: 7,
+        day: 'Saturday',
+        total: 20
+    },
+    {
+        id: 8,
+        day: 'Sunday',
+        total: 30
+    },
+]
+
+export const TotalCustomerPerBranch = [
+    {
+        id: 1,
+        day: 'Jakarta Selatan 1',
+        total: 100
+    },
+    {
+        id: 2,
+        day: 'Jakarta Timur 4',
+        total: 80
+    },
+    {
+        id: 4,
+        day: 'Bandung 3',
+        total: 60
+    },
+    {
+        id: 5,
+        day: 'Bandung 1',
+        total: 70
+    },
+    {
+        id: 6,
+        day: 'Bogor 2',
+        total: 80
+    },
+    {
+        id: 7,
+        day: 'Depok 3',
+        total: 60
+    },
+    {
+        id: 8,
+        day: 'Jakarta Selatan 4',
+        total: 75
+    },
+]
