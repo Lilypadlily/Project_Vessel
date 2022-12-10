c528ef5ccca395cbdff9cbe4cf309bc59d0edb95
avalokita <devi.avalokita@ansell.com>
2022-12-10 21:25:34 +0700

    Add confimation password for registration

--- basic-admin-template-vessel-main/src/views/pages/register/Register.js
+++ basic-admin-template-vessel-main/src/views/pages/register/Register.js
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from 'react'
+import { React, useState, useEffect } from 'react'
 import {
   CButton,
   CCard,
@@ -16,35 +16,55 @@ import { cilLockLocked, cilUser } from '@coreui/icons'
 
 import axios from "axios";
 const Register = () => {
+  const [loginStatus, setLoginStatus] = useState();
   const [userData, setUserData] = useState({
     username: "",
     fullName: "",
     password: "",
+    passwordConfirmation: "",
   })
 
   const handleChange = (event) => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
     // setUserData[name] = event.target.value;
     setUserData((prev) => {
       return {
         ...prev,
         [name]: value
       }
+      
     })
+
+    // if (name === "password" || name === "passwordConfirmation") {
+    //   if (userData.password === userData.passwordConfirmation) {
+    //     setLoginStatus(1)
+    //   } else {
+    //     setLoginStatus(0);
+    //   }
+    // }
+
     //  console.log(userData);
   }
 
   const handleSubmit = () => {
-      const InputUserData = async (userData) => {
-          const res = await axios.post("http://localhost:3000/api/InsertNewUser", userData);
-      }
+    const InputUserData = async (userData) => {
+      const res = await axios.post("http://localhost:3000/api/InsertNewUser", userData);
+    }
+    if (userData.password === userData.passwordConfirmation) {
+      setLoginStatus();
       InputUserData(userData);
       console.log("User inputted");
       setUserData({
         username: "",
         fullName: "",
         password: "",
+        passwordConfirmation: ""
       })
+    } else {
+      setLoginStatus(0);
+      console.log("Password is not the same");
+    }
+
   }
 
   return (
@@ -61,11 +81,11 @@ const Register = () => {
                     <CInputGroupText>
                       <CIcon icon={cilUser} />
                     </CInputGroupText>
-                    <CFormInput placeholder="Nama" autoComplete="username" name="fullName" value={userData.fullName} onChange={handleChange}/>
+                    <CFormInput placeholder="Nama" autoComplete="username" name="fullName" value={userData.fullName} onChange={handleChange} />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" name="username" value={userData.username} onChange={handleChange}/>
+                    <CFormInput placeholder="Email" autoComplete="email" name="username" value={userData.username} onChange={handleChange} />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
@@ -86,8 +106,12 @@ const Register = () => {
                       type="password"
                       placeholder="Konfirmasi password"
                       autoComplete="new-password"
+                      name="passwordConfirmation"
+                      value={userData.passwordConfirmation}
+                      onChange={handleChange}
                     />
                   </CInputGroup>
+                  {(loginStatus === 0) && <p style={{color: 'red'}}>Password dan konfirmasi password berbeda. Mohon cek ulang.</p>} 
                   <div className="d-grid">
                     <CButton color="dark" onClick={handleSubmit}>Daftar</CButton>
                   </div>
